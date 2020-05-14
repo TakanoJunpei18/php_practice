b2eb38a0f0ea53ba69581326d3d787814a877478
”junpei” <jun.desu@au.com>
2020-05-14 10:14:40 +0000

    first commit

---
+++ practice.php
@@ -0,0 +1,33 @@
+<?php
+echo 'hello php!';
+$name ="jun";
+
+if($name="jun") {
+    echo "私はjunです";
+}else{
+    echo "私はjunではありません";
+
+
+?>
+  
+<?php
+
+for($i = 0; $i<10000; $i=++){
+    echo $i;
+}
+?>
+
+<?php
+
+$fruits = array("rinngo","suika","banana","meron","remon");
+
+foreach($fruits as $fruit ){
+    echo "要素は" . $fruit;
+    echo　"\n";
+}
+?>
+
+
+
+
+
