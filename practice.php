120b2336982643fa3c0608cafa2083956fed2f5d
”junpei” <jun.desu@au.com>
2020-05-15 12:28:08 +0000

    second commit

--- practice.php
+++ practice.php
@@ -1,33 +1,40 @@
-<?php
-echo 'hello php!';
-$name ="jun";
 
-if($name="jun") {
-    echo "私はjunです";
-}else{
-    echo "私はjunではありません";
+<?php
+//1 $aという変数に3を、$bという変数に７を代入して、echoを使って$a+$bの結果を表示してみましょう。
+
+$a=3;
+$b=7;
+echo $a+$b;
+
+//2 $array_month という配列に1月〜12月の文字列を代入し、echoで8月を表示してみましょう。
+
+
+echo $array_month = ["January","February","March","April","May","June","July", 
+                 "August","September","October","November","December"];
+                 
+echo $array_month[7];
+
+
+
+
+//3 $helloという変数に”Hello, “を $name という変数にあなたの名前を、$world という変数に”‘s World!” を代入して、
+//  各変数を連結させて “Hello,あなたの名前’s World!” と表示してて、各変数を連結させて “Hello,あなたの名前’s World!” と表示してみましょう。
+
+ $hello="Hello,";
+ $name="jun";
+ $world="'World";
+ 
+ echo $hello.$name.$world;
+
+//4 $tech_boostに “tech “という文字列が代入されています。複合演算を使って、
+//  echo $tech_boostと実行すると「tech boost」と表示されるように実装してみましょう
+
+$tech_boost="tech";
+$tech_boost.="boost";
+echo $tech_boost
 
 
 ?>
-  
-<?php
-
-for($i = 0; $i<10000; $i=++){
-    echo $i;
-}
-?>
-
-<?php
-
-$fruits = array("rinngo","suika","banana","meron","remon");
-
-foreach($fruits as $fruit ){
-    echo "要素は" . $fruit;
-    echo　"\n";
-}
-?>
-
-
 
 
 
---
+++ test.php
@@ -0,0 +1,41 @@
+// $a　という変数に3を、$b　という変数に７を代入して、echoを使って$a+$bの結果を表示してみましょう。
+<?php
+$a="3";
+$b="7";
+echo $a+$b;
+
+
+
+
+echo 'hello php!';
+$name ="jun";
+
+if($name="jun") {
+    echo "私はjunです";
+}else{
+    echo "私はjunではありません";
+
+
+
+  
+
+
+for($i = 0; $i<10000; $i=++){
+    echo $i;
+}
+
+
+
+
+$fruits = array("rinngo","suika","banana","meron","remon");
+
+foreach($fruits as $fruit ){
+    echo "要素は" . $fruit;
+    echo　"\n";
+}
+?>
+
+
+
+
+
