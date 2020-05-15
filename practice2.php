86542ea468b80220b6a256c088f1c3cc87318061
”junpei” <jun.desu@au.com>
2020-05-15 14:14:13 +0000

    third commit

--- practice2.php
+++ practice2.php
@@ -38,3 +38,4 @@ echo $tech_boost
 
 
 
+
---
+++ practice3.php
@@ -0,0 +1,35 @@
+
+<?php
+//1 $name にあなたの名前を代入し、 if文で $name があなたの名前だったら 「私は あなたの名前 です」、
+//  もし違ったら「あなたの名前ではありません」と表示するように実装してください。
+
+$name ="jun";
+
+if($name="jun") {
+    echo "私はjunです";
+}else{
+    echo "私はjunではありません";
+
+
+}
+//2 for文を使って、1から10000までの合計の値を表示してください
+
+for($i = 1; $i < 10000 ; $i++){
+    echo $i;
+}
+
+
+
+
+$fruits = array("rinngo","suika","banana","meron","remon");
+
+foreach($fruits as $fruit ){
+    echo "要素は".$fruit;
+    echo "\n";
+}
+
+?>
+
+
+
+
