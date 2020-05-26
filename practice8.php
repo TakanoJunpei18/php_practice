6ba742aeeb42262217fe95d009fddffddce07699
”junpei” <jun.desu@au.com>
2020-05-26 16:50:32 +0000

    first commit

---
+++ practice8.php
@@ -0,0 +1,25 @@
+<?php
+//1  ControllerとRoutingについてわからないことを書き出してメンターに質問してみましょう。
+
+
+
+
+//2  Controllerの役割について、説明してください。
+//modelとviewの仲介役
+
+
+
+//3  ControllerとRoutingの役割について、説明してください。
+//RoutingはユーザーからのアクセスをControllerに送るために必要なもの
+//Controllerはmodelとviewの仲介役であり連携を取り合うのには必要不可欠なもの
+
+
+
+
+
+
+
+
+
+
+?>
