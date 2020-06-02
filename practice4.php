71b248edd97e2996bfc15c91748e661affa7a20b
”junpei” <jun.desu@au.com>
2020-06-02 13:42:59 +0000

    sixth commit

--- practice4.php
+++ practice4.php
@@ -1,11 +1,10 @@
 <?php
 //1.引数に数値を指定して実行すると、数値を2倍にして返す関数を作成してください
 
-function yyy( $Number ){
-
-echo $Number * 2;
+function yyy( $Number ) {
+    return $Number * 2;
 }
-yyy( 1 );
+echo yyy( 1 );
 
 
 
