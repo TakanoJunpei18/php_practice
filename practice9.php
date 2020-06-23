97257dabc3f3732d85daec264ee9db6eb2a3ab50
”junpei” <jun.desu@au.com>
2020-06-23 03:38:02 +0000

    sixth commit

--- practice9.php
+++ practice9.php
@@ -1,10 +1,13 @@
-<?php
-//1 URLとControllerやActionを紐付ける機能を何といいますか？
-//Routing
+<?php>
+/**1 URLとControllerやActionを紐付ける機能を何といいますか？
 
-//2 あなたが考える、group化をすることのメリットを考えてみてください。
-//コードが見やすくなる。
-//同じコードを書く手間がはぶける
+Routingの説明
+アクセスしたアドレスに応じて対応するControllerのActionを呼び出す仕組みのこと。
+
+2 あなたが考える、group化をすることのメリットを考えてみてください。
+
+コードが見やすくなる
+同じコードを書く手間がはぶける */
 
 
 //3 「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
@@ -19,9 +22,9 @@ Route::group(['prefix' => 'admin'], function() {
 });
 
 //3  「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
-Route::group(['prefix' => 'XXX'], function() {
-    Route::get('XXX','XXX\AAAController@bbb');
-});   
+
+Route:get('XXX', 'AAAController\bbb');
+ 
 //4 応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。
 Route::group(['prefix' => 'admin'], function() {   
     Route::get('profile/create','Admin\ProfileController@add');
